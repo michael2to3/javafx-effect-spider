5e641d4e80d6cf85241736515446060591306ef5
Michael <michael2ga3@gmail.com>
2023-02-13 15:00:56 +0300

    Point has field opacity

--- app/src/main/java/javafx/effect/spider/PointGraphics.java
+++ app/src/main/java/javafx/effect/spider/PointGraphics.java
@@ -3,17 +3,26 @@ package javafx.effect.spider;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.paint.Color;
 
-class PointGraphics {
-    Point point;
-    Color color;
-    final double timelife;
-    double restlife;
+public class PointGraphics {
+    private Point point;
+    private Color color;
+    private final double timelife;
+    private double restlife;
+    private double opacity;
 
     public PointGraphics(Point point, Color color, double timelife) {
         this.point = point;
         this.color = color;
         this.timelife = timelife;
         this.restlife = timelife;
+    }
+
+    public double getOpacity() {
+        return opacity;
+    }
+
+    public void setOpacity(double opacity) {
+        this.opacity = opacity;
     }
 
     public Point getPoint() {
@@ -44,9 +53,14 @@ class PointGraphics {
         this.restlife = restlife;
     }
 
+    public void updateOpacity() {
+        this.opacity = 1 - (timelife - restlife) / timelife;
+    }
+
     public void draw(Canvas canvas) {
+        updateOpacity();
         var ctx = canvas.getGraphicsContext2D();
-        var ncolor = color.deriveColor(0, 1, 1, 1 - (timelife - restlife) / timelife);
+        var ncolor = color.deriveColor(0, 1, 1, this.opacity);
         ctx.setFill(ncolor);
 
         ctx.fillOval(point.getX() - point.getR(), point.getY() - point.getR(), point.getR() * 2, point.getR() * 2);

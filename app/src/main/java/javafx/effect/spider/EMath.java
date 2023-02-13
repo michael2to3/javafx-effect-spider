9b8d3abfe089550fd6d9e1e359bc18e9c45f81f3
Michael <michael2ga3@gmail.com>
2023-02-13 19:39:17 +0300

    Add dist method for math

--- app/src/main/java/javafx/effect/spider/EMath.java
+++ app/src/main/java/javafx/effect/spider/EMath.java
@@ -8,4 +8,8 @@ class EMath {
         }
         return ans;
     }
+
+    public static double distance(final double x, final double y, final double tx, final double ty) {
+        return Math.sqrt(Math.pow(x - tx, 2) + Math.pow(y - ty, 2));
+    }
 }
